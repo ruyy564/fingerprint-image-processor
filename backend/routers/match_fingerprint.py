63f27c8e059955aa91b1005f23eba1c84b19b247
Константин Климкин <bluemars@yandex.ru>
2024-05-05 17:09:31 +0300

    добавлено api для сравнения изображений

--- backend/routers/match_fingerprint.py
+++ backend/routers/match_fingerprint.py
@@ -1,65 +1,92 @@
-import os
+import base64
+import io
 from typing import List
 import cv2
 from dotenv import load_dotenv
-from fastapi import Depends, APIRouter, HTTPException, Response, UploadFile, File
-
+from fastapi import Depends, APIRouter, HTTPException, UploadFile
+import numpy as np
+from fastapi.responses import JSONResponse
 from utils import validate_cloudflare, aws_bucket_name, s3
-
-routerMatchFingerprint = APIRouter()
+import zipfile
 
 load_dotenv()
 
+routerMatchFingerprint = APIRouter()
 
-@routerMatchFingerprint.get("/match-fingerprints")
-async def match_fingerprints(user=Depends(validate_cloudflare)) -> List[str]:
-    absolute_path = os.path.join(
-        'backend/dataset/SOCOFing/Altered/Altered-Hard/150__M_Right_index_finger_Obl.BMP')
-    sample = cv2.imread(absolute_path)
 
-    prefix = f'{user['id']}/'
-    file_list = s3.list_objects(Bucket=aws_bucket_name, Prefix=prefix)
+@routerMatchFingerprint.post("/match-fingerprints")
+async def match_fingerprints(filename: str, file: UploadFile, user=Depends(validate_cloudflare)) -> List[str]:
+    try:
+        # чтение загруженного изображения
+        contents = await file.read()
+        nparr = np.fromstring(contents, np.uint8)
+        sample = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    image, filename = None, None
-    kp1, kp2, mp = None, None, None
+        # получение выбранного zip-файла
+        prefix = f'{user['id']}/'
+        content = s3.get_object(Bucket=aws_bucket_name,
+                                Key=f'{prefix}{filename}')['Body'].read()
 
-    best_score = 0
-    count = 0
-    for file in [file for file in os.listdir(os.path.join("backend/dataset/SOCOFing/Real/"))][:1000]:
-        fingerprint_image = cv2.imread(os.path.join(
-            "backend/dataset/SOCOFing/Real/") + file)
-        sift = cv2.SIFT.create()
+        image, filename = None, None
+        kp1, kp2, mp = None, None, None
+        best_score = 0
 
-        if (count % 10 == 0):
-            print(count)
-        count += 1
+        # чтение изображений из zip-файла и сравнение их с загруженным
+        with zipfile.ZipFile(io.BytesIO(content), 'r') as zip:
+            for item in zip.namelist():
+                # чтение изображение из архива
+                content = zip.read(item)
+                nparr = np.fromstring(content, np.uint8)
+                fingerprint_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-        keypoints_1, descriptors_1 = sift.detectAndCompute(sample, None)
-        keypoints_2, descriptors_2 = sift.detectAndCompute(
-            fingerprint_image, None)
+                # инициализация sift
+                sift = cv2.SIFT.create()
 
-        matches = cv2.FlannBasedMatcher({'algorithm': 1, 'trees': 10},
-                                        {}).knnMatch(descriptors_1, descriptors_2, k=2)
+                # получение сегментов изображений
+                keypoints_1, descriptors_1 = sift.detectAndCompute(
+                    sample, None)
+                keypoints_2, descriptors_2 = sift.detectAndCompute(
+                    fingerprint_image, None)
 
-        match_points = []
-        for p, q in matches:
-            if p.distance < 0.1 * q.distance:
-                match_points.append(p)
+                # сравнение изображений с помощью FlannBasedMatcher
+                matches = cv2.FlannBasedMatcher({'algorithm': 1, 'trees': 10},
+                                                {}).knnMatch(descriptors_1, descriptors_2, k=2)
 
-        keypoints = 0
-        if len(keypoints_1) < len(keypoints_2):
-            keypoints = len(keypoints_1)
-        else:
-            keypoints = len(keypoints_2)
+                # преобразование реззультатов в численное выражение
+                match_points = []
+                for p, q in matches:
+                    if p.distance < 0.1 * q.distance:
+                        match_points.append(p)
 
-        if len(match_points) / keypoints * 100 > best_score:
-            best_score = len(match_points) / keypoints * 100
-            filename = file
-            image = fingerprint_image
-            kp1, kp2, mp = keypoints_1, keypoints_2, match_points
+                keypoints = 0
+                if len(keypoints_1) < len(keypoints_2):
+                    keypoints = len(keypoints_1)
+                else:
+                    keypoints = len(keypoints_2)
 
-    print('BEST MATCH: ' + str(filename))
-    print('SCORE: ' + str(best_score))
+                if len(match_points) / keypoints * 100 > best_score:
+                    best_score = len(match_points) / keypoints * 100
+                    filename = file.filename
+                    image = fingerprint_image
+                    kp1, kp2, mp = keypoints_1, keypoints_2, match_points
 
-    result = cv2.drawMatches(sample, kp1, image, kp2, mp, None)
-    result = cv2.resize(result, None, fx=4, fy=4)
+        # создание изображение, которое показывает схожие сегменты сравниваемых изображений
+        result = cv2.drawMatches(sample, kp1, image, kp2, mp, None)
+        result = cv2.resize(result, None, fx=4, fy=4)
+
+        # преобразование результата в base64
+        _, encoded_img = cv2.imencode('.PNG', result)
+        encoded_img = base64.b64encode(encoded_img)
+
+        return JSONResponse(
+            content={
+                "SCORE": str(best_score),
+                "BEST MATCH": str(filename),
+                "result": str(encoded_img)
+            },
+            status_code=200
+        )
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=403, detail="The Matching fingerprint is not available")
