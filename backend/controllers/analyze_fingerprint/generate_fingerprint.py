4062130d89d07e874517095c69defbc8ff992cac
Константин Климкин <bluemars@yandex.ru>
2024-05-12 19:55:23 +0300

    создание api для генерации изображений

---
+++ backend/controllers/analyze_fingerprint/generate_fingerprint.py
@@ -0,0 +1,58 @@
+import io
+import zipfile
+import cv2
+from fastapi import HTTPException, Response
+from numpy import expand_dims
+from keras_preprocessing.image import img_to_array
+from keras_preprocessing.image import ImageDataGenerator
+import numpy as np
+
+
+# сравнение загруженного изображения отпечатка пальца с выбранным набором из zip-файла
+async def generate_fingerprints(rotation_range, horizontal_flip, width_shift_range, height_shift_range, zoom_range, count_generated_image, file):
+    try:
+        # чтение загруженного изображения
+        contents = await file.read()
+
+        # Создание zip для сгенерированных ихобюражений
+        o = io.BytesIO()
+        zf = zipfile.ZipFile(o, mode='w')
+
+        # чтение изображений из zip-файла
+        with zipfile.ZipFile(io.BytesIO(contents), 'r') as zip:
+            for filename in zip.namelist():
+                # чтение изображение из архива и его подготовка для его изменения
+                contents = zip.read(filename)
+                nparr = np.fromstring(contents, np.uint8)
+                nparr = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+                data = img_to_array(nparr)
+                samples = expand_dims(data, 0)
+
+                # Инициализация генератора изображений
+                datagen = ImageDataGenerator(rotation_range=rotation_range, horizontal_flip=horizontal_flip, width_shift_range=width_shift_range, height_shift_range=height_shift_range, zoom_range=zoom_range)
+
+                # Создание итератора для генератора изображений
+                it = datagen.flow(samples, batch_size=1)
+                for i in range(count_generated_image):
+                    batch = it.next()
+                    image = batch[0].astype('uint8')
+
+                    # Добавление изображения в zip
+                    zf.writestr(f'{i}_{filename}', cv2.imencode(
+                        '.jpg', image)[1].tobytes())
+        zf.close()
+        o.seek(0)
+        response = o.read()
+        o.close()
+
+        return Response(
+            content=response,
+            headers={
+                'Content-Disposition': f'attachment;filename=generated',
+                'Content-type': 'application/x-zip-compressed'
+            }
+        )
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=403, detail="Image generator error")
--- backend/routers/analyze_fingerprint.py
+++ backend/routers/analyze_fingerprint.py
@@ -1,8 +1,8 @@
-from fastapi import Depends, APIRouter, UploadFile
+from fastapi import Depends, APIRouter, File, Form, UploadFile
 
 from utils.validate_user_token import validate_user_token
-
 from controllers.analyze_fingerprint.match_fingerprints import match_fingerprints
+from controllers.analyze_fingerprint.generate_fingerprint import generate_fingerprints
 
 
 router_analyze_fingerprints = APIRouter(prefix='/analyze')
@@ -12,3 +12,18 @@ router_analyze_fingerprints = APIRouter(prefix='/analyze')
 @router_analyze_fingerprints.post("/match-fingerprints")
 async def route_match_fingerprints(filename: str, file: UploadFile, user=Depends(validate_user_token)):
     return await match_fingerprints(filename, file, user)
+
+
+# сравнение загруженного изображения отпечатка пальца с выбранным набором из zip-файла
+@router_analyze_fingerprints.post("/generate-fingerprints")
+async def route_match_fingerprints(
+    rotation_range: float | None = Form(90),
+    horizontal_flip: bool | None = Form(False),
+    width_shift_range: tuple[float, float] | None = Form(None),
+    height_shift_range: float | None = Form(None),
+    zoom_range: float | None = Form(0.),
+    count_generated_image: int | None = Form(9),
+    file: UploadFile = File(...),
+):
+       
+    return await generate_fingerprints(rotation_range, horizontal_flip, width_shift_range, height_shift_range, zoom_range, count_generated_image, file)
